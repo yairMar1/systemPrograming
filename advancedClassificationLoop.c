349106d9f5c8b89f8c2336643426a56694cbe18c
yairMar1 <156588104+yairMar1@users.noreply.github.com>
2024-01-28 14:34:58 +0200

    Add files via upload
    update 28/1/24 14:34

--- advancedClassificationLoop.c
+++ advancedClassificationLoop.c
@@ -9,28 +9,33 @@ int powerOf10(int exponent) {
     return result;
 }  
  
- int isPalindrome (int x){
+int isPalindrome(int x) {
     int counter = 0;
     int y = x;
     int z = x;
     int p = x;
 
-    while(y>=1){// how many digit the number has
-        y/=10;
+    while (y >= 1) { // how many digits the number has
+        y /= 10;
         counter++;
     }
 
-    for (int i = 0 ;i < counter ; i++){
-        int h = p%10;//start at the last digit
-        int b = (z/ powerOf10(counter -1 -i));;//star at the first digit
+    for (int i = 0; i < counter; i++) {
+        int h = p % 10; // start at the last digit
+        int b = (z / powerOf10(counter - 1 - i)); // start at the first digit
 
-        if(h != b){return 0;}
-        p/=10;
-        z = (z/ powerOf10(counter -1 -i))%10;
-    }    
+        if (h != b) {
+            return 0;
+        }
+
+        p /= 10;
+        z = (z % powerOf10(counter - 1 - i));
+    }
 
     return 1;
- }
+}
+
+
  
  int power(int base, int exponent) {
     if (exponent == 0) {return 1;}
@@ -44,23 +49,26 @@ int powerOf10(int exponent) {
     }
 }
  
- int isArmstrong (int x){
+ int isArmstrong(int x) {
     int counter = 0;
     int y = x;
     int z = x;
-    
-    while(y>=1){
-        y/=10;
+
+    while (y >= 1) {
+        y /= 10;
         counter++;
     }
+
     int arm = 0;
 
-    for(int i = 1; i <= counter; i++){
-        int sum = z%10;
+    for (int i = 1; i <= counter; i++) {
+        int sum = z % 10;
         arm += power(sum, counter);
-        if(x < arm){return 0;}
-        z/10;
+        if (x < arm) {
+            return 0;
+        }
+        z /= 10;  // corrected syntax
     }
-    
+
     return (arm == x) ? 1 : 0;
- }
+}
--- main.c
+++ main.c
@@ -6,36 +6,35 @@ int main(){
 int number1 = 0;
 int number2 = 0;
 
-printf("enter a number1: ");
+//printf("enter a number1: ");
 scanf("%d", &number1);
 
-printf("enter a number2: ");
+//printf("enter a number2: ");
 scanf("%d", &number2);
 
-
-printf("The Prime numbers are:");
+printf("The Armstrong numbers are: ");
 for(int i= number1; i<=number2;i++){
-    if(isPrime(i)){printf("%d",i);}
-    printf(" ");}
+    if(isArmstrong(i)){printf("%d ",i);}
+    }
     printf("\n");
 
-printf("The Armstrong numbers are:");
+printf("The Palindromes are: ");
 for(int i= number1; i<=number2;i++){
-    if(isArmstrong(i)){printf("%d",i);}
-    printf(" ");}
-    printf("\n");
-
-printf("The Strong numbers are:");
-for(int i= number1; i<=number2;i++){
-    if(isStrong(i)){printf("%d",i);}
-    printf(" ");}
-    printf("\n");
-
-printf("The Palindromes are:");
-for(int i= number1; i<=number2;i++){
-    if(isPalindrome(i)){printf("%d",i);}
-    printf(" ");}
+    if(isPalindrome(i)){printf("%d ",i);}
+    }
 printf("\n");
+
+printf("The Prime numbers are: ");
+for(int i= number1; i<=number2;i++){
+    if(isPrime(i)){printf("%d ",i);}
+    }
+    printf("\n");
+
+printf("The Strong numbers are: ");
+for(int i= number1; i<=number2;i++){
+    if(isStrong(i)){printf("%d ",i);}
+}
+    printf("\n");
 
 return 0;
 }
